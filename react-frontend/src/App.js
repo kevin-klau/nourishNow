73d54843a68f5efe9441b2fac695472d3269a0ba
lyriao <96245364+lyriao@users.noreply.github.com>
2023-03-18 16:51:57 -0400

    webstart

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-center-horizontal">
+      <div className="home">
+        <h1 className="title inknutantiqua-normal-black-55px">
+          <span className="inknutantiqua-normal-black-55px">M</span>
+          <span className="span1">ealMapper</span>
+        </h1>
+        <div className = "overlap-group">
+          <img className = "image-1" src="image-1.png" alt="image 1" />
+          <div className = "ellipse-3"></div>
+          <img className = "image-2" src="image-2.png" alt="image 2" />
+          <img className = "image-3" src="image-3.png" alt="image 3" />
+          <div className="rectangle-2"></div>
+          <div className="demo">
+            Demo
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
