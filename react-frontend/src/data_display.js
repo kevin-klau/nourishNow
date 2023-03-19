0afe9aa4a645ce860c98838133954eee6580c37c
Kevin Lau <Kevin.lau15@yahoo.com>
2023-03-18 23:07:39 -0400

    Added Buttons Changing Scroll

--- react-frontend/src/data_display.js
+++ react-frontend/src/data_display.js
@@ -1,7 +1,16 @@
 import React, { useEffect, useState } from "react";
 
 export default function data({state}){
+    function clickBack(){
+        document.getElementById('title').scrollIntoView();
+    }
     return(
-        <h1>{state}</h1>
+        <div id="DataInfo">
+            <div className="row">
+                <div className="col-lg-1"></div>
+                <button className="btn btn-light col-lg-2" id="button_home" onClick={clickBack}><strong>Search New State</strong></button>
+                <h1 id="state_header" className="col-lg-6">{state} Stats</h1>
+            </div>
+        </div>
     );
 }
