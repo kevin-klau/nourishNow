986853b621b4ff69ec382766a6bc3777eae3e3ef
Kevin Lau <Kevin.lau15@yahoo.com>
2023-03-18 20:52:58 -0400

    Started to create website, added interactive map

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,14 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
-import React from "react";
-import image from "./images/image-1.jpg"
-import plane from "./images/image-2.png"
+import React, { useEffect, useState } from "react";
+import USAMap from "react-usa-map";
+import abb from "./state_transfer"
+
+
 
 function App() {
+  const [text, setText] = useState('insert state')
+
+  const handleClick = (event) => {
+    let abbreviate = event.target.dataset.name;
+    setText(abb(abbreviate));
+  }
+  
   return (
-    <div id="website" className="container-center-horizontal">
+    <>
+    <div id="website"  className="container-center-horizontal">
       <h1 id="title">Meal Mapper</h1>
     </div>
+    <div id="map">
+      <USAMap
+        onClick={handleClick}
+      />
+    </div>
+    </>
   );
 }
 
