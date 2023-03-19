53e7d1fabca3077c8980b72392e467c96de9301f
Kevin Lau <Kevin.lau15@yahoo.com>
2023-03-19 07:29:21 -0400

    Updated Recipes

--- react-frontend/src/search.js
+++ react-frontend/src/search.js
@@ -25,11 +25,11 @@ export default function UserSearch({state}){
         const response = completion.data.choices[0].message.content+"HIHI";
         console.log(response);
         let response1 = response.replace(/\n/g,"<br>");
-        let response2 = response1.replace("Ingredients:","<h2 id='IngredientsTitle'><strong>Ingredients:</strong></h2><h4 id='Ingredients'>");
-        let response3 = response2.replace("Instructions:","</h4><h2 id='IntructionsTitle'><strong>Instructions:</strong></h2><h4 id='Instructions'>")
-        let response4 = response3.replace("Directions:","</h4><h2 id='DirectionsTitle'><strong>Directions:</strong></h2><h4 id='Directions'>")
+        let response2 = response1.replace("Ingredients:","<div class='row' style={{marginBottom:'-50px'}}><div class='col-lg-4'><h2 id='IngredientsTitle'><strong>Ingredients:</strong></h2><h4 id='Ingredients'>");
+        let response3 = response2.replace("Instructions:","</div><div class='col-lg-8'></h4><h2 id='IntructionsTitle'><strong>Instructions:</strong></h2><h4 id='Instructions'>")
+        let response4 = response3.replace("Directions:","</div><div class='col-lg-8'></h4><h2 id='DirectionsTitle'><strong>Directions:</strong></h2><h4 id='Directions'>")
         
-        let response5 = response4.replace("HIHI","</h4>")
+        let response5 = response4.replace("HIHI","</h4></div></div>")
         document.getElementById('k').innerHTML = (response5);
 
     };
@@ -49,7 +49,7 @@ export default function UserSearch({state}){
                 <input onChange={userType} id="input" className="form-control me-2 d-flex clear" type="search" placeholder="Enter Subject Here" aria-label="Search"></input>
                 <button id="inputButton" className="btn btn-outline-success" type="submit" onClick={userEnter}>🔎︎</button>
             </form>
-            <p id="k" style={{ overflowWrap: "break-word", maxWidth: "100%" }}></p>
+            <div id="k" style={{ overflowWrap: "break-word", maxWidth: "100%" }}></div>
             
         </div>
     );
