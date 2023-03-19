d25878120d4527954eb2d164bb26a04842247c5c
Kevin Lau <Kevin.lau15@yahoo.com>
2023-03-19 00:43:02 -0400

    Added more better looking ui

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import USAMap from "react-usa-map";
 import abb from "./state_transfer";
 import Data from "./data_display";
+import image1 from "./images/circle.png";
 
 function App() {
   let gpt_response = "HAIDHOWAHI";
@@ -43,13 +44,20 @@ function App() {
     <head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"/>
     </head>
-    <div id="website"  className="container-center-horizontal">
-      <h1 id="title">Meal Mapper</h1>
-    </div>
-    <div id="map">
-      <USAMap
-        onClick={(event) => handleClick(event.target.dataset.name)}
-      />
+    <div className='rectangle1'/>
+    <div className='rectangle2'/>
+    <div id="backgroundImages">
+      <div id="website"  className="container-center-horizontal">
+        <h1 id="title" style={{display:'inline-block'}}><strong>Meal <h1 id="titleBlue" style={{display:'inline-block'}}>Mapper</h1></strong></h1>
+      </div>
+        <div className="container-center-horizontal">
+          <h2 id="description"> Select a state down below to view the food scarcity statistics</h2>
+        </div>
+      <div id="map">
+        <USAMap
+          onClick={(event) => handleClick(event.target.dataset.name)}
+        />
+      </div>
     </div>
     {/*<h1 id = "gpt_result"></h1>*/}
     <Data id="theData" state={text}></Data>
